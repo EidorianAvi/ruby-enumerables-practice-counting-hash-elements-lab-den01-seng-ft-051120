04080e3e4e12edd0c0d9e3b078b564e5d8a60d8b
Rene Avila <reneavila1993@gmail.com>
2020-05-16 18:20:32 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,7 +1,10 @@
 def count_elements(array)
   hash = {}
   array.each do |element|
-    hash[element]
+    hash[element] = 1 
+    if hash[element] == hash[element]
+      hash[element] += 1 
+    end
   end
   hash
 end
